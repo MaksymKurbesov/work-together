d265b2a91a1d15a102f01ff61986c8fe5c8f28bc
unknown <bonyklade@gmail.com>
2024-03-12 08:59:09 +0200

    added adaptive

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "src/vacancies/index.html"),
+        vacancies: resolve(__dirname, "src/vacancies/index.html"),
       },
     },
   },
